0bbe3003afe574ae7d92ee3b5d78c39f76ef318a
Mohammad Javad <javadhosseini14@gmail.com>
2025-05-12 18:39:14 +0330

    add price validation utility and enhance invoice total section with icons and styled text

--- app/src/main/java/com/example/composetrainer/ui/screens/invoice/invoicescreen/BottomTotalSection.kt
+++ app/src/main/java/com/example/composetrainer/ui/screens/invoice/invoicescreen/BottomTotalSection.kt
@@ -4,22 +4,33 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetrainer.R
+import com.example.composetrainer.ui.theme.BComps
+import com.example.composetrainer.ui.theme.BHoma
+import com.example.composetrainer.ui.theme.BKoodak
+import com.example.composetrainer.ui.theme.BLotus
+import com.example.composetrainer.ui.theme.BNazanin
 import com.example.composetrainer.ui.theme.ComposeTrainerTheme
 import com.example.composetrainer.utils.dimen
 import com.example.composetrainer.utils.str
+import com.example.composetrainer.utils.PriceValidator
+import com.example.composetrainer.utils.dimenTextSize
 
 @Composable
 fun BottomTotalSection(
@@ -49,14 +60,19 @@ fun BottomTotalSection(
                 ),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = str(R.string.total),
-                style = MaterialTheme.typography.titleMedium
+            Icon(
+                painter = painterResource(id = R.drawable.toman),
+                contentDescription = "Date",
+                modifier = Modifier
+                    .size(dimen(R.dimen.size_lg))
             )
 
             Text(
-                text = "$totalPrice",
+                text = PriceValidator.formatPrice(totalPrice.toString()),
                 style = MaterialTheme.typography.titleMedium,
+                fontFamily = BHoma,
+                fontWeight = FontWeight.Bold,
+                fontSize = dimenTextSize(R.dimen.text_size_4xl),
                 modifier = Modifier.padding(start = dimen(R.dimen.space_2))
             )
 
@@ -65,12 +81,15 @@ fun BottomTotalSection(
             Button(
                 onClick = onSubmit,
                 modifier = Modifier.padding(start = dimen(R.dimen.space_2)),
-                enabled = hasItems && !isLoading
+                enabled = hasItems && !isLoading,
+                shape = RoundedCornerShape(dimen(R.dimen.radius_md))
             ) {
                 Text(
-                    text = str(R.string.submit),
-                    style = MaterialTheme.typography.titleMedium
-                )
+                    text = str(R.string.finalize_factor),
+                    fontFamily = BHoma,
+                    fontSize = dimenTextSize(R.dimen.text_size_xxl),
+                    modifier = Modifier.padding(
+                        vertical = dimen(R.dimen.space_2), horizontal = dimen(R.dimen.space_4)))
             }
         }
     }
