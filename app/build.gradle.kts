a5039b58467b9f6d3dfe51f0cc94e53e54865612
MJavad <javadhosseini14@gmail.com>
2025-05-27 01:03:05 +0330

    feat: upgrade Version Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.composetrainer"
         minSdk = 25
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
