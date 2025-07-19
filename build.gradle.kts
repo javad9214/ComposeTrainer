b721b79b8297ead06e154ad53702c656a69932da
Mohammad Javad <javadhosseini14@gmail.com>
2025-07-19 10:32:02 +0330

    success build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     alias(libs.plugins.android.library) apply false
     id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }
