384ffe154a363a34cfc2c949a3ba0ffaebfbf0a7
Mohammad Javad <javadhosseini14@gmail.com>
2025-09-20 16:26:21 +0330

    feat: adding the remote connect structure

---
+++ app/src/main/java/com/example/composetrainer/data/remote/api/ApiConstants.kt
@@ -0,0 +1,12 @@
+package com.example.composetrainer.data.remote.api
+
+import com.example.composetrainer.BuildConfig
+
+object ApiConstants {
+
+    const val BASE_URL = BuildConfig.BASE_URL
+
+    const val API = "api/"
+
+
+}
--- app/src/main/java/com/example/composetrainer/data/remote/dto/PagedResponse.kt
+++ app/src/main/java/com/example/composetrainer/data/remote/dto/PagedResponse.kt
@@ -1,23 +1,10 @@
 package com.example.composetrainer.data.remote.dto
 
-import com.google.gson.annotations.SerializedName
-
-data class PagedResponse<T>(
-    @SerializedName("content")
+data class PagedResponseDto<T>(
     val content: List<T>,
-
-    @SerializedName("page")
     val page: Int,
-
-    @SerializedName("size")
     val size: Int,
-
-    @SerializedName("totalElements")
     val totalElements: Long,
-
-    @SerializedName("totalPages")
     val totalPages: Int,
-
-    @SerializedName("last")
     val last: Boolean
 )
