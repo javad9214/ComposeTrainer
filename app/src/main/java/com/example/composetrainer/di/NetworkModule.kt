37b6a07c917e365b5edcd1c1e0079f15a99fa42c
MJavad <javadhosseini14@gmail.com>
2025-12-08 19:24:35 +0330

    fix: only replace auth log after token

--- app/src/main/java/com/example/composetrainer/di/NetworkModule.kt
+++ app/src/main/java/com/example/composetrainer/di/NetworkModule.kt
@@ -33,8 +33,8 @@ object NetworkModule {
             .connectTimeout(300, TimeUnit.SECONDS)  // Connection timeout
             .readTimeout(300, TimeUnit.SECONDS)     // Read timeout
             .writeTimeout(300, TimeUnit.SECONDS)    // Write timeout
-            .addInterceptor(loggingInterceptor)
             .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
             .build()
     }
 
